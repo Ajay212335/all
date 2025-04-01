2e3716a965bfcfd7aa329113f3abd6db87281389
Ajay212335 <ajaiks2005@gmail.com>
2025-04-02 00:00:20 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -541,7 +541,10 @@ def image():
         return "Image uploaded successfully!", 200
     except Exception as e:
         return str(e), 500
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no environment variable
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
